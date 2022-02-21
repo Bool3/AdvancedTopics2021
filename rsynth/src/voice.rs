d3c3947326efbf604d2fa091574bdbd98a8412b6
Julian Sarkissian <pixlkid24@gmail.com>
2022-02-20 22:30:15 -0800

    blep with square working hopefully?

--- rsynth/src/voice.rs
+++ rsynth/src/voice.rs
@@ -8,10 +8,11 @@ const PI: f32 = std::f32::consts::PI;
 const TWO_PI: f32 = 2.0 * std::f32::consts::PI;
 
 pub struct RVoice {
-    frequency: f32,         // Hz
+    frequency: f32,             // Hz
     pub enabled: bool, 
-    pub phase: f32,         // range: [0, TWO_PI)
-    phase_increment: f32,   // range: [0, PI]
+    pub phase: f32,             // range: [0, TWO_PI)
+    phase_increment: f32,       // range: [0, PI]
+    blep_splice_length: f32,    // phase_increment * 4.0
 }
 
 
@@ -19,6 +20,16 @@ fn note_to_frequency(note_number: u8) -> f32 {
     return 440.0 * 2.0_f32.powf((note_number as f32 - 69.0) / 12.0);
 }
 
+fn blep_down(phase: f32) -> f32 {
+    // only the first harmonic and third harmonic of the step, multiplied by 3/2 to normalize
+    // domain: [0, PI]
+    (f32::cos(phase) - f32::cos(phase * 3.0) / 3.0) * (3.0 / 2.0)
+}
+
+fn blep_up(phase: f32) -> f32 {
+    // step_up is just the step_down flipped top to bottom
+    blep_down(phase) * -1.0
+}
 
 impl RVoice {
     pub fn new(note_number: u8) -> RVoice {
@@ -27,9 +38,11 @@ impl RVoice {
             enabled: false,
             phase: 0.0,
             phase_increment: 0.0,
+            blep_splice_length: 0.0,
         };
         
         new_voice.update_phase_increment(44100.0);
+        new_voice.update_blep_splice_length(4.0);
         
         return new_voice;
     }
@@ -38,6 +51,13 @@ impl RVoice {
         let cycles_per_sample = self.frequency / sample_rate;
         
         self.phase_increment = cycles_per_sample * TWO_PI;
+
+    }
+
+    pub fn update_blep_splice_length(&mut self, samples_long: f32) {
+        // the band limited step will be "samples_long" samples long, but
+        // will contain "samples_long + 1" samples (i.e. includes sample 0)
+        self.blep_splice_length = self.phase_increment * samples_long;
     }
     
     pub fn process(&mut self, wave: Wave) -> f32 {
@@ -53,21 +73,22 @@ impl RVoice {
                     1.0 + (-2.0 * f32::abs(self.phase - PI)) / PI
                 },
                 Wave::Square => {
-                    // phase is within two samples of discontinuity at PI
-                    if f32::abs(PI - self.phase) <= 2.0 * self.phase_increment {
-                        self.band_limited_step(self.phase)
+                    // near upwards discontinuity (~0)
+                    if self.phase <= self.blep_splice_length {
+                        // we sample the blep function at a fraction of its length (PI)
+                        // that fraction being the phase / blep_splice_length : [0, 1]
+                        // (remember: the blep_splice_length is some multiple of the phase_increment)
+                        blep_up(self.phase / self.blep_splice_length * PI)
                     
-                    // phase is within the two samples to the LEFT of dicontinuity at 0 (TWO_PI)
-                    } else if self.phase >= TWO_PI - 2.0*self.phase_increment {
-                        self.band_limited_step(self.phase - PI + TWO_PI)
-
-                    // phase is within the two samples to the RIGHT of discontinuity at 0 (TWO_PI)
-                    } else if self.phase <= 2.0*self.phase_increment {
-                        self.band_limited_step(self.phase + PI + TWO_PI)
-
+                    // between upwards discontinuity and downwards discontinuity
                     } else if self.phase < PI {
                         1.0
+                    
+                    // near downwards discontinuity (~PI)
+                    } else if self.phase <= PI + self.blep_splice_length {
+                        blep_down((self.phase - PI) / self.blep_splice_length * PI)
 
+                    // between downwards discontinuity and upwards discontinuity
                     } else {
                         -1.0
                     }
@@ -96,19 +117,6 @@ impl RVoice {
         if self.phase > TWO_PI {
             self.phase -= TWO_PI;
         }
-    }
-
-    fn band_limited_step(&self, phase: f32) -> f32 {
-        let period = self.phase_increment * 4.0;
-
-        // the coefficient of our input to the cosine function such that the cosine has our period
-        let mut period_coefficient = TWO_PI / period;
-
-        // stretches the period by 2 -- without this, the step only lasts from 0 to PI
-        period_coefficient /= 2.0;
-
-        // only the first harmonic and third harmonic of the step, multiplied by 3/2 to normalize
-        3.0 / 2.0 * (f32::cos(phase * period_coefficient) - f32::cos(phase * period_coefficient * 3.0) / 3.0)
     }
 }
 
