275214a5c2603fec2c22e4f27fbfb963b66004f2
Julian Sarkissian <pixlkid24@gmail.com>
2022-03-06 20:17:45 -0800

    tweaks

--- rsynth/src/voice.rs
+++ rsynth/src/voice.rs
@@ -36,16 +36,10 @@ impl RVoice {
     }
 
     pub fn play(&mut self, velocity: u8) {
-
-        self.is_on = true;
-        self.envelope.start(velocity);
-
-
-        /*
         if velocity != 0 {
             self.is_on = true;
             self.envelope.start(velocity);
-        }*/
+        }
     }
 
     pub fn stop(&mut self) {
